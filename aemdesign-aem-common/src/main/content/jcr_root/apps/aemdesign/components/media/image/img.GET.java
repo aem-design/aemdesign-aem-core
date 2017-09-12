c73642cd588976aee701bf9fefb9257cacb1644b
Max Barrass <max.barrass@gmail.com>
2017-09-12 23:03:55 +1000

    update namespace

--- aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/media/image/img.GET.java
+++ aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/media/image/img.GET.java
@@ -1,16 +1,5 @@
-/*
- * Copyright 1997-2008 Day Management AG
- * Barfuesserplatz 6, 4001 Basel, Switzerland
- * All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Day Management AG, ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Day.
- */
 
-package libs.foundation.components.parbase;
+package aemdesign.components.media.image;
 
 import com.day.cq.commons.ImageHelper;
 import com.day.cq.commons.ImageResource;
