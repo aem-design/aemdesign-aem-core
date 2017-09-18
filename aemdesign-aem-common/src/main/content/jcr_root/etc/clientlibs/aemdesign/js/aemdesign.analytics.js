6002b4a78b32c1aa7a2dcfd088b21eb7e68209b0
Max Barrass <max.barrass@gmail.com>
2017-09-18 23:17:06 +1000

    cleanup declarations

--- aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.analytics.js
+++ aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.analytics.js
@@ -12,44 +12,54 @@ window.AEMDESIGN.analytics = AEMDESIGN.analytics || {};
         return _version;
     };
 
+    ns.getAnalyticsData = function($element) {
+        return {
+            "componentid"               : $element.attr("componentid"), //component id
+            "href"                      : $element.attr("href"), //event
+            "target"                    : $element.attr("target") || "", //event
+            "analyticsType"             : $element.attr("data-analytics-type") || "", //event
+            "analyticsHitType"          : $element.attr("data-analytics-hit-type") || "", //hit / type
+            "analyticsEventCategory"    : $element.attr("data-analytics-event-category") || "", //eg Link Click
+            "analyticsEventAction"      : $element.attr("data-analytics-event-action") || "", //eg Content, Navigation, Email, Phone
+            "analyticsEventLabel"       : $element.attr("data-analytics-event-label") || "", //title of link
+            "analyticsTransport"        : $element.attr("data-analytics-transport") || "", //beacon
+            "analyticsNonInteraction"   : $element.attr("data-analytics-noninteraction") || "" //hidden transactions
+        }
+    };
+
     ns.getAnalyticsInfoAA = function($object) {
 
+        var data = ns.getAnalyticsData($object);
+
         var pageTitle = document.title;
-        var linkLabel = $object.attr("data-label") || $object.text().trim(); //link title
-        var hitType = $object.attr("data-analytics-hit-type"); //eg link_o ...
-        var eventLabel = linkLabel || $object.attr("data-analytics-event-label"); //title of link
-        var eventCategory = $object.attr("data-analytics-event-category"); //eg ${Page Title}:${Link Click}:${Link Title}
+        var linkLabel = data.analyticsEventLabel || $object.text().trim(); //link title
+        var eventLabel = linkLabel || data.analyticsEventLabel; //title of link
 
         var linkEvars = {
-            event: hitType,
-            category: [pageTitle,eventCategory,eventLabel].join(":"),
+            event: data.analyticsHitType,
+            category: [pageTitle,data.analyticsEventCategory,eventLabel].join(":"),
             label: eventLabel
         };
         return linkEvars;
     };
 
     ns.getAnalyticsInfoGA = function($object) {
-        var hitType = $object.attr("data-analytics-hit-type"); //event
-        var eventCategory = $object.attr("data-analytics-event-category"); //eg Link Click
-        var eventAction = $object.attr("data-analytics-event-action"); //eg Content, Navigation, Email, Phone
-        var eventLabel = $object.attr("data-analytics-event-label"); //title of link
-        var eventTransport = $object.attr("data-analytics-transport"); //beacon
-        var eventNonInteraction = $object.attr("data-analytics-noninteraction"); //hidden transactions
 
+        var data = ns.getAnalyticsData($object);
 
         var linkEvars = {
-            hitType: hitType,
-            eventCategory: eventCategory,
-            eventAction: eventAction,
-            eventLabel: eventLabel
+            hitType: data.analyticsHitType,
+            eventCategory: data.analyticsEventCategory,
+            eventAction: data.analyticsEventAction,
+            eventLabel: data.analyticsEventLabel
         };
 
-        if (eventNonInteractive) {
-            linkEvars["nonInteraction"] = eventNonInteraction;
+        if (data.analyticsNonInteraction) {
+            linkEvars["nonInteraction"] = data.analyticsNonInteraction;
         }
 
-        if (eventTransport) {
-            linkEvars["transport"] = eventTransport;
+        if (data.analyticsTransport) {
+            linkEvars["transport"] = data.analyticsTransport;
         }
         return linkEvars;
     };
@@ -94,26 +104,39 @@ window.AEMDESIGN.analytics = AEMDESIGN.analytics || {};
 
     ns.enableButton = function($button,reset) {
 
-        var href = $button.attr("href"); //event
-        var target = $button.attr("target") || ""; //event
-        var analyticsType = $button.attr("data-analytics-type") || ""; //event
+        var data = ns.getAnalyticsData($button);
 
         if (reset) {
             $button.attr('onclick', '');
         }
 
-        if (analyticsType==="aa") {
+        if (data.analyticsType==="aa") {
             ns.enableAA($button);
-        } else if (analyticsType==="ga") {
+        } else if (data.analyticsType==="ga") {
             ns.enableGA($button);
         }
 
-        var onClick = new Function('window.open("'+ window.encodeURI(href)+'","'+target+'")');
+        var onClick = new Function('window.open("'+ window.encodeURI(data.href)+'","'+data.target+'")');
 
         $button.click(onClick);
 
 
     };
+
+    ns.imageLoad = function($image) {
+
+        var data = ns.getAnalyticsData($image);
+
+        if (data.analyticsType==="aa") {
+            if (assetAnalytics) {
+                assetAnalytics.core.assetLoaded($image)
+            }
+        } else if (data.analyticsType==="ga") {
+
+        }
+
+    };
+
 
 })(AEMDESIGN.jQuery, _, ko, AEMDESIGN.analytics, AEMDESIGN.log, this); //pass in additional dependencies
 
