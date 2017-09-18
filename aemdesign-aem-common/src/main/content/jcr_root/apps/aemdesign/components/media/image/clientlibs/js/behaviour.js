5be2cb561361668ffff0e585932a54356c6d17fc
Max Barrass <max.barrass@gmail.com>
2017-09-18 23:17:54 +1000

    add analytics events

--- aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/media/image/clientlibs/js/behaviour.js
+++ aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/media/image/clientlibs/js/behaviour.js
@@ -4,14 +4,30 @@ window.AEMDESIGN.components = AEMDESIGN.components || {};
 window.AEMDESIGN.components.image = AEMDESIGN.components.image || {};
 
 
-(function ($, _, ko, ns, window, undefined) { //add additional dependencies
+(function ($, _, ko, ns, analytics, window, undefined) { //add additional dependencies
 
     $(window).on("load resize", function () {
-        $("div[data-picture]").each(function () {
-            var picture = $(this);
-            ns.picturefill(picture);
+        $("div.image[data-analytics-type=aa]").each(function() {
+            var link = $(this).find("a");
+            if (link) {
+                analytics.enableAA(link);
+            }
         });
+        $("div.image[data-analytics-type=ga]").each(function() {
+            var link = $(this).find("a");
+            if (link) {
+                analytics.enableGA(link);
+            }
+        });
+        $("div.image").load(function() {
+            analytics.imageLoad($(this));
+        });
+        //depreciated using picturefill.js
+        // $("div[data-picture]").each(function () {
+        //     var picture = $(this);
+        //     ns.picturefill(picture);
+        // });
 
     });
 
-})(AEMDESIGN.jQuery, _, ko, AEMDESIGN.components.image, this); //pass in additional dependencies
+})(AEMDESIGN.jQuery, _, ko, AEMDESIGN.components.image, AEMDESIGN.analytics, this); //pass in additional dependencies
