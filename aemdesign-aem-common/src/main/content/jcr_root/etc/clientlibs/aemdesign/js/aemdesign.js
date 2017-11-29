0ad33f1259d91697d5f90fb75e44f82ec78181cc
Max Barrass <max.barrass@gmail.com>
2017-11-29 16:41:31 +1100

    split ajax setup into own file and order after http init

--- aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.js
+++ aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.js
@@ -1,29 +1,10 @@
 //aemdesign.js - Namespace
-window.AEMDESIGN = window.AEMDESIGN || {"jQuery":{}};
-window.AEMDESIGN.jQuery = window.jQuery || {};
-window.AEMDESIGN.$ = window.jQuery || $;
 
-(function ($, ns, http, window, undefined) {
+//create namespace
+(function (window, undefined) {
 
-    "use strict";
-    var _version = "0.1";
+    window.AEMDESIGN = window.AEMDESIGN || {"jQuery":{}};
+    window.AEMDESIGN.jQuery = window.jQuery || {};
+    window.AEMDESIGN.$ = window.jQuery || $;
 
-    ns.version = function () {
-        return _version;
-    };
-
-    $.ajaxSetup({ // necessary global modifications for ajax calls
-        statusCode: {
-            403: function(jqXHR) {
-                if (jqXHR.getResponseHeader("X-Reason") === "Authentication Failed") {
-                    // login session expired: redirect to login page
-                    AEMDESIGN.http.handleLoginRedirect(); //access directly as this is first class that runs
-                }
-            }
-        }
-    });
-
-    $.ajaxSettings.traditional = true;
-
-
-})(AEMDESIGN.jQuery, AEMDESIGN, AEMDESIGN.http, this);
+}(this));
