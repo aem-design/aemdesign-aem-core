d40853dcf82da332240fedf524660ef0f6a69525
Max Barrass <max.barrass@gmail.com>
2017-12-03 18:25:11 +1100

    update client libs for use in touchui

---
+++ aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/clientlibs-author-coralui-authoring/js/modals.js
@@ -0,0 +1,13 @@
+//clientlibs-author-coralui-authoring - modals
+(function ($) {
+    'use strict';
+
+    // make sure modals are displayed in the middle of the page when in preview mode
+    $('.modal').on('show.bs.modal', function () {
+        var viewportHeight = parent.innerHeight;
+        $('.modal-dialog').css('top', viewportHeight * 0.5);
+    });
+
+    console.log("clientlibs-author-coralui-authoring - modals");
+
+}(jQuery));
