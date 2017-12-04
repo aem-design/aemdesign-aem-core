27ecbf68c6f1d5546dd7918c977125cce46f8dea
Max Barrass <max.barrass@gmail.com>
2017-12-05 00:47:56 +1100

    update to use aemdesign/components/layout/container

--- aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.sling.js
+++ aemdesign-aem-common/src/main/content/jcr_root/etc/clientlibs/aemdesign/js/aemdesign.sling.js
@@ -273,7 +273,7 @@ window.AEMDESIGN.sling = window.AEMDESIGN.sling || {};
         PAGE_CONTENT: "jcr:content",
         PATH_PARSYS: "par",
 
-        RESOURCE_PARSYS: "aemdesign/components/common/parsys",
+        RESOURCE_PARSYS: "aemdesign/components/layout/container",
 
         FIELD_SLING_RESOURCE_TYPE: "sling:resourceType",
         FIELD_JCR_PRIMARY_TYPE: "jcr:primaryType",
