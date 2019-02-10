694209b71c9965a4dabcac1b1ccad0f33899b604
Max Barrass <max.barrass@gmail.com>
2019-02-10 23:57:03 +1100

    add article model

---
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/layout/Article.java
@@ -0,0 +1,50 @@
+package design.aem.models.v2.layout;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.i18n.I18n;
+import design.aem.components.ComponentProperties;
+import design.aem.utils.components.ComponentsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static design.aem.utils.components.ComponentsUtil.*;
+import static design.aem.utils.components.ImagesUtil.getBackgroundImageRenditions;
+
+public class Article extends WCMUsePojo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Article.class);
+
+    private ComponentProperties componentProperties = null;
+    public ComponentProperties getComponentProperties() {
+        return this.componentProperties;
+    }
+
+    @Override
+    public void activate() throws Exception {
+
+        com.day.cq.i18n.I18n _i18n = new I18n(getRequest());
+
+
+        final String DEFAULT_ARIA_ROLE = "article";
+
+        // {
+        //   { name, defaultValue, attributeName, valueTypeClass }
+        // }
+        Object[][] componentFields = {
+                {FIELD_ARIA_ROLE,DEFAULT_ARIA_ROLE, DEFAULT_ARIA_ROLE_ATTRIBUTE},
+                {FIELD_VARIANT, DEFAULT_VARIANT},
+        };
+
+        componentProperties = ComponentsUtil.getComponentProperties(
+                this,
+                componentFields,
+                DEFAULT_FIELDS_STYLE,
+                DEFAULT_FIELDS_ACCESSIBILITY);
+
+        componentProperties.putAll(getBackgroundImageRenditions(this));
+
+    }
+
+
+
+}
