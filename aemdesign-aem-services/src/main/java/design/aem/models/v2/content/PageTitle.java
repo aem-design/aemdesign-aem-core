b0c1cbb34abd69419af248556773121c8bf13302
Max Barrass <max.barrass@gmail.com>
2019-02-12 12:10:46 +1100

    convert pagetitle to htl

---
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/content/PageTitle.java
@@ -0,0 +1,51 @@
+package design.aem.models.v2.content;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.i18n.I18n;
+import design.aem.components.ComponentProperties;
+import design.aem.utils.components.ComponentsUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jackrabbit.vault.util.JcrConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static design.aem.utils.components.ComponentsUtil.*;
+
+public class PageTitle extends WCMUsePojo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PageTitle.class);
+
+    private ComponentProperties componentProperties = null;
+    public ComponentProperties getComponentProperties() {
+        return this.componentProperties;
+    }
+
+    @Override
+    public void activate() throws Exception {
+
+        I18n _i18n = new I18n(getRequest());
+
+        final String DEFAULT_TAG_TYPE = "span";
+
+        Object[][] componentFields = {
+                {FIELD_VARIANT, DEFAULT_VARIANT},
+                {JcrConstants.JCR_TITLE, ""},
+                {FIELD_TITLE_TAG_TYPE, DEFAULT_TAG_TYPE}
+        };
+
+        componentProperties = ComponentsUtil.getComponentProperties(
+                this,
+                componentFields,
+                DEFAULT_FIELDS_STYLE,
+                DEFAULT_FIELDS_ACCESSIBILITY,
+                DEFAULT_FIELDS_DETAILS_OPTIONS);
+
+        String jcrTitle = getPageProperties().get(JcrConstants.JCR_TITLE, "");
+        String overrideTitle = componentProperties.get(JcrConstants.JCR_TITLE, "");
+
+        componentProperties.put("pagetitle", StringUtils.isEmpty(overrideTitle) ? jcrTitle : overrideTitle);
+    }
+
+
+
+}
