519b2c0f4c62e9c2284c4755753f2ba2f0c7bc2a
Max Barrass <max.barrass@gmail.com>
2019-02-15 23:31:10 +1100

    remove logs update to use java.util.List class

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/NavList.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/NavList.java
@@ -71,19 +71,13 @@ public class NavList extends WCMUsePojo {
         String[] supportedDetails = DEFAULT_LIST_DETAILS_SUFFIX;
         String[] supportedRoots = DEFAULT_LIST_PAGE_CONTENT;
 
-        LOGGER.error("NAVLIST: {}","test");
-        LOGGER.error("NAVLIST: {}","test");
-
-        List<ComponentProperties> pagesInfo = null;
+        java.util.List<ComponentProperties> pagesInfo = null;
         if (componentProperties.get("listFrom", DEFAULT_LISTFROM).equals(LISTFROM_CHILDREN)) {
-            LOGGER.error("NAVLIST: listFrom: {}",LISTFROM_CHILDREN);
-
             Page parentPage = getPageManager().getPage(componentProperties.get("parentPage", ""));
             if (parentPage != null) {
                 pagesInfo = getPageListInfo(getContextObjects(this),getPageManager(), getResourceResolver(), parentPage.listChildren(), supportedDetails, supportedRoots, 2, true);
             }
         } else {
-            LOGGER.error("NAVLIST: listFrom: {}", componentProperties.get("listFrom", "not set"));
             String[] paths = componentProperties.get("pages", new String[0]);
             if (paths.length != 0) {
                 pagesInfo = getPageListInfo(getContextObjects(this),getPageManager(), getResourceResolver(), paths, supportedDetails, supportedRoots, 2, true);
@@ -100,181 +94,181 @@ public class NavList extends WCMUsePojo {
     }
 
 
-
-    /**
-     * Get a list of page {title, href ...} info
-     *
-     * @param pageManager resolver to get the pages at the paths
-     * @param paths string array of paths
-     * @return
-     */
-    protected List<Map> getMenuPageList(PageContext pageContext, PageManager pageManager, ResourceResolver resourceResolver, String[] paths, Page currentPage, SlingHttpServletRequest req, boolean isThemeExists) throws RepositoryException {
-        List<Map> pages = new ArrayList<Map>();
-
-
-        for (String path : paths) {
-            //Page child = resolver.getResource(path).adaptTo(Page.class);
-            Page child = pageManager.getPage(path);
-
-            if (child != null) {
-                Map infoStruct = new HashMap();
-
-                //grab page info
-//                Resource contentRes = child.getContentResource();
-//                Node contentNode = contentRes.adaptTo(Node.class);
-
-                infoStruct.putAll(getPageLinkInfo(child));
-
-                Resource parSys = child.getContentResource("par");
-                Boolean hasChildren = false;
-                if (parSys != null) {
-                    Node parSysNode = parSys.adaptTo(Node.class);
-                    if (parSysNode.hasNodes()) {
-                        hasChildren = true;
-                        infoStruct.put("parsysPath", parSys.getPath());
-                    } else {
-                        infoStruct.put("parsysPath", "");
-                    }
-                } else {
-                    infoStruct.put("parsysPath", "");
-                }
-
-                infoStruct.putAll(getPageInfo(pageContext, child,resourceResolver,null,null));
-
-                //grab page details info
-//                infoStruct.putAll(getPageDetailsInfo(child));
-
-
-                //grab children for current 2nd level of current path
-                String currentPath = currentPage != null ? currentPage.getPath() : "";
-
-                infoStruct.put("currentPath", currentPath);
-
-
-                if (StringUtils.isNotEmpty(currentPath)) {
-
-                    //if menu item is of same level as current path then grab its children
-                    String section = Text.getAbsoluteParent(currentPath, 3);
-
-                    infoStruct.put("current", section.equals(child.getPath()));
-                    if (!isThemeExists) {
-                        infoStruct.put("menuColor", getMenuColor(req, child));
-                    }
-
-                    List<Map> children = getChildren(child, currentPath, 3, req, isThemeExists);
-                    infoStruct.put("children", children);
-
-                    hasChildren = children.size()!=0;
-
-                }
-
-
-                String noContentClass = (hasChildren ? "" : "no-content");
-
-                infoStruct.put("cssClass", StringUtils.join("page-", child.getName().trim(), noContentClass, " "));
-
-                pages.add(infoStruct);
-            }
-        }
-
-        return pages;
-    }
-
-    /**
-     * recursevley build a tree for page up defined by levels
-     * @param page starting page
-     * @param currentPath curret path used to determined active pages
-     * @param level level to gather
-     * @param req
-     * @return
-     */
-
-    private List<Map> getChildren(Page page, String currentPath, int level, SlingHttpServletRequest req, boolean isThemeExists) {
-        List<Map> childrenList = new ArrayList<Map>();
-
-        if (page != null && level-- > 0) {
-            Iterator<Page> children = page.listChildren(new PageFilter(req));
-
-            while (children.hasNext()) {
-                Page nextchild = children.next();
-                //skip if hidden
-                if (!nextchild.isHideInNav()) {
-                    String childPath = nextchild.getPath();
-
-                    Map info = getPageLinkInfo(nextchild);
-
-                    boolean current = false;
-                    if (currentPath.equals(childPath)) {
-                        current = true;
-                    } else if (currentPath.startsWith(childPath+"/")) {
-                        current = true;
-                    } else if (currentPath.indexOf(childPath+"/") > 0) {
-                        current = true;
-                    }
-
-                    info.put("current", current);
-                    if (!isThemeExists) {
-                        info.put("menuColor", getMenuColor(req, nextchild));
-                    }
-
-                    if (level > 0) {
-                        //keep going
-                        info.put("children", getChildren(nextchild, currentPath, level, req, isThemeExists));
-                    }
-
-                    childrenList.add(info);
-                }
-            }
-        }
-
-        return childrenList;
-    }
-
-    /***
-     * get information about the page
-     * @param page
-     * @return
-     */
-
-    private Map getPageLinkInfo(Page page) {
-        Map info = new HashMap();
-
-        if (page != null) {
-            ValueMap childVM = page.getProperties();
-
-            info.put("title", getPageNavTitle(page));
-
-            String vanityPath = StringUtils.defaultIfEmpty(page.getVanityUrl(), "");
-
-            info.put("vanityPath", vanityPath);
-
-            String redirectTarget = page.getPath();
-
-            if (childVM.containsKey("redirectTarget")) {
-                redirectTarget = childVM.get("redirectTarget", "");
-            }
-
-            //respect redirect targets
-            if (redirectTarget.contains("http:")) {
-                info.put("href", redirectTarget);
-            } else {
-                info.put("href", redirectTarget.concat(DEFAULT_EXTENTION));
-//                //respect vanity URLs
-//                if (isNotEmpty(vanityPath)){
-//                    info.put("href", vanityPath);
+//
+//    /**
+//     * Get a list of page {title, href ...} info
+//     *
+//     * @param pageManager resolver to get the pages at the paths
+//     * @param paths string array of paths
+//     * @return
+//     */
+//    protected List<Map> getMenuPageList(PageContext pageContext, PageManager pageManager, ResourceResolver resourceResolver, String[] paths, Page currentPage, SlingHttpServletRequest req, boolean isThemeExists) throws RepositoryException {
+//        List<Map> pages = new ArrayList<Map>();
+//
+//
+//        for (String path : paths) {
+//            //Page child = resolver.getResource(path).adaptTo(Page.class);
+//            Page child = pageManager.getPage(path);
+//
+//            if (child != null) {
+//                Map infoStruct = new HashMap();
+//
+//                //grab page info
+////                Resource contentRes = child.getContentResource();
+////                Node contentNode = contentRes.adaptTo(Node.class);
+//
+//                infoStruct.putAll(getPageLinkInfo(child));
+//
+//                Resource parSys = child.getContentResource("par");
+//                Boolean hasChildren = false;
+//                if (parSys != null) {
+//                    Node parSysNode = parSys.adaptTo(Node.class);
+//                    if (parSysNode.hasNodes()) {
+//                        hasChildren = true;
+//                        infoStruct.put("parsysPath", parSys.getPath());
+//                    } else {
+//                        infoStruct.put("parsysPath", "");
+//                    }
 //                } else {
-//                    info.put("href", redirectTarget.concat(DEFAULT_EXTENTION));
+//                    infoStruct.put("parsysPath", "");
 //                }
-            }
-
-            info.put("authHref", page.getPath().concat(DEFAULT_EXTENTION));
-            info.put("pathHref", page.getPath());
-
-        }
-
-        return info;
-
-    }
+//
+//                infoStruct.putAll(getPageInfo(pageContext, child,resourceResolver,null,null));
+//
+//                //grab page details info
+////                infoStruct.putAll(getPageDetailsInfo(child));
+//
+//
+//                //grab children for current 2nd level of current path
+//                String currentPath = currentPage != null ? currentPage.getPath() : "";
+//
+//                infoStruct.put("currentPath", currentPath);
+//
+//
+//                if (StringUtils.isNotEmpty(currentPath)) {
+//
+//                    //if menu item is of same level as current path then grab its children
+//                    String section = Text.getAbsoluteParent(currentPath, 3);
+//
+//                    infoStruct.put("current", section.equals(child.getPath()));
+//                    if (!isThemeExists) {
+//                        infoStruct.put("menuColor", getMenuColor(req, child));
+//                    }
+//
+//                    List<Map> children = getChildren(child, currentPath, 3, req, isThemeExists);
+//                    infoStruct.put("children", children);
+//
+//                    hasChildren = children.size()!=0;
+//
+//                }
+//
+//
+//                String noContentClass = (hasChildren ? "" : "no-content");
+//
+//                infoStruct.put("cssClass", StringUtils.join("page-", child.getName().trim(), noContentClass, " "));
+//
+//                pages.add(infoStruct);
+//            }
+//        }
+//
+//        return pages;
+//    }
+//
+//    /**
+//     * recursevley build a tree for page up defined by levels
+//     * @param page starting page
+//     * @param currentPath curret path used to determined active pages
+//     * @param level level to gather
+//     * @param req
+//     * @return
+//     */
+//
+//    private List<Map> getChildren(Page page, String currentPath, int level, SlingHttpServletRequest req, boolean isThemeExists) {
+//        List<Map> childrenList = new ArrayList<Map>();
+//
+//        if (page != null && level-- > 0) {
+//            Iterator<Page> children = page.listChildren(new PageFilter(req));
+//
+//            while (children.hasNext()) {
+//                Page nextchild = children.next();
+//                //skip if hidden
+//                if (!nextchild.isHideInNav()) {
+//                    String childPath = nextchild.getPath();
+//
+//                    Map info = getPageLinkInfo(nextchild);
+//
+//                    boolean current = false;
+//                    if (currentPath.equals(childPath)) {
+//                        current = true;
+//                    } else if (currentPath.startsWith(childPath+"/")) {
+//                        current = true;
+//                    } else if (currentPath.indexOf(childPath+"/") > 0) {
+//                        current = true;
+//                    }
+//
+//                    info.put("current", current);
+//                    if (!isThemeExists) {
+//                        info.put("menuColor", getMenuColor(req, nextchild));
+//                    }
+//
+//                    if (level > 0) {
+//                        //keep going
+//                        info.put("children", getChildren(nextchild, currentPath, level, req, isThemeExists));
+//                    }
+//
+//                    childrenList.add(info);
+//                }
+//            }
+//        }
+//
+//        return childrenList;
+//    }
+//
+//    /***
+//     * get information about the page
+//     * @param page
+//     * @return
+//     */
+//
+//    private Map getPageLinkInfo(Page page) {
+//        Map info = new HashMap();
+//
+//        if (page != null) {
+//            ValueMap childVM = page.getProperties();
+//
+//            info.put("title", getPageNavTitle(page));
+//
+//            String vanityPath = StringUtils.defaultIfEmpty(page.getVanityUrl(), "");
+//
+//            info.put("vanityPath", vanityPath);
+//
+//            String redirectTarget = page.getPath();
+//
+//            if (childVM.containsKey("redirectTarget")) {
+//                redirectTarget = childVM.get("redirectTarget", "");
+//            }
+//
+//            //respect redirect targets
+//            if (redirectTarget.contains("http:")) {
+//                info.put("href", redirectTarget);
+//            } else {
+//                info.put("href", redirectTarget.concat(DEFAULT_EXTENTION));
+////                //respect vanity URLs
+////                if (isNotEmpty(vanityPath)){
+////                    info.put("href", vanityPath);
+////                } else {
+////                    info.put("href", redirectTarget.concat(DEFAULT_EXTENTION));
+////                }
+//            }
+//
+//            info.put("authHref", page.getPath().concat(DEFAULT_EXTENTION));
+//            info.put("pathHref", page.getPath());
+//
+//        }
+//
+//        return info;
+//
+//    }
 
 
     /***
@@ -291,26 +285,26 @@ public class NavList extends WCMUsePojo {
 
         return "";
     }
-
-    private String getMenuColor(SlingHttpServletRequest request, Page page) {
-        if (page == null) {
-            return null;
-        }
-        String menuColor = DETAILS_MENU_COLOR_DEFAULT;
-        try {
-            Node detailNode = findDetailNode(page);
-
-            if (detailNode != null) {
-                if (detailNode.hasProperty(DETAILS_MENU_COLOR)) {
-                    menuColor = detailNode.getProperty(DETAILS_MENU_COLOR).getString();
-                }
-            }
-        } catch (Exception ex) {
-
-        }
-
-        return "default".equalsIgnoreCase(menuColor) ? "" : menuColor;
-    }
+//
+//    private String getMenuColor(SlingHttpServletRequest request, Page page) {
+//        if (page == null) {
+//            return null;
+//        }
+//        String menuColor = DETAILS_MENU_COLOR_DEFAULT;
+//        try {
+//            Node detailNode = findDetailNode(page);
+//
+//            if (detailNode != null) {
+//                if (detailNode.hasProperty(DETAILS_MENU_COLOR)) {
+//                    menuColor = detailNode.getProperty(DETAILS_MENU_COLOR).getString();
+//                }
+//            }
+//        } catch (Exception ex) {
+//
+//        }
+//
+//        return "default".equalsIgnoreCase(menuColor) ? "" : menuColor;
+//    }
 
 
 
