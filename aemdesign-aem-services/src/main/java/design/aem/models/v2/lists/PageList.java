b301fad9d00949ca8981474606e7b656d0731b50
Max Barrass <max.barrass@gmail.com>
2019-02-17 21:12:51 +1100

    read labels for component

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/PageList.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/PageList.java
@@ -1,12 +1,18 @@
 package design.aem.models.v2.lists;
 
 import com.day.cq.i18n.I18n;
+import design.aem.components.ComponentProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static design.aem.utils.components.I18nUtil.*;
 
 public class PageList extends List  {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PageList.class);
+
+    private final String DEFAULT_I18N_CATEGORY = "pagelist";
+
 
     @Override
     public void activate() throws Exception {
@@ -18,6 +24,10 @@ public class PageList extends List  {
         detailsNameSuffix = new String[]{"page-details"};
 
         loadConfig();
+
+        //override properties
+        getComponentProperties().put("listItemLinkText", getDefaultLabelIfEmpty("",DEFAULT_I18N_CATEGORY,DEFAULT_I18N_LIST_ITEM_LINK_TEXT,DEFAULT_I18N_CATEGORY,_i18n));
+        getComponentProperties().put("listItemLinkTitle", getDefaultLabelIfEmpty("",DEFAULT_I18N_CATEGORY,DEFAULT_I18N_LIST_ITEM_LINK_TITLE,DEFAULT_I18N_CATEGORY,_i18n));
 
     }
 
