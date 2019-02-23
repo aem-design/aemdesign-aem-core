8239d178c029c7d155806abd405bae3c586e6644
Max Barrass <max.barrass@gmail.com>
2019-02-23 11:59:35 +1100

    add template helper model

---
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/template/Template.java
@@ -0,0 +1,41 @@
+package design.aem.models.v2.template;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.i18n.I18n;
+import com.day.cq.wcm.api.Page;
+import design.aem.components.ComponentProperties;
+import design.aem.utils.components.ComponentsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static design.aem.utils.components.CommonUtil.DEFAULT_LIST_DETAILS_SUFFIX;
+import static design.aem.utils.components.CommonUtil.findComponentInPage;
+
+public class Template extends WCMUsePojo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Template.class);
+
+    private ComponentProperties componentProperties = null;
+    public ComponentProperties getComponentProperties() {
+        return this.componentProperties;
+    }
+
+    @Override
+    public void activate() throws Exception {
+
+        I18n _i18n = new I18n(getRequest());
+
+        componentProperties = ComponentsUtil.getNewComponentProperties(this);
+
+        String[] listLookForDetailComponent = DEFAULT_LIST_DETAILS_SUFFIX;
+        Page componentPage = getPageManager().getContainingPage(getResource().getPath());
+        String detailsPath = findComponentInPage(getCurrentPage(),listLookForDetailComponent);
+        String componentPath = detailsPath + ".badge.metadata";
+
+        componentProperties.put("detailsPath",detailsPath);
+        componentProperties.put("detailsMetadataBadgePath",componentPath);
+    }
+
+
+
+}
