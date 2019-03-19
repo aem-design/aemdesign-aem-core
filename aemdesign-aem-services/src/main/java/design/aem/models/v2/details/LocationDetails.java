15bcaca333486ab42d5ec473d0b92e902c18b5c1
wildone <max.barrass@gmail.com>
2019-03-19 13:22:00 +1100

    add analytics tabs

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/details/LocationDetails.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/details/LocationDetails.java
@@ -58,6 +58,7 @@ public class LocationDetails extends GenericDetails {
                 componentFields,
                 DEFAULT_FIELDS_STYLE,
                 DEFAULT_FIELDS_ACCESSIBILITY,
+                DEFAULT_FIELDS_ANALYTICS,
                 DEFAULT_FIELDS_DETAILS_OPTIONS);
 
         String[] tags = componentProperties.get(TagConstants.PN_TAGS, new String[]{});
