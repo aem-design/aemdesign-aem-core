7077bbce506502f5e477bb1a1d126b4a6728b10e
wildone <max.barrass@gmail.com>
2019-03-19 16:56:51 +1100

    force platform.twitter.com/widgets.js to use https as http does not work

--- aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/widgets/twitter/clientlibs/js/behaviour.js
+++ aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/widgets/twitter/clientlibs/js/behaviour.js
@@ -12,7 +12,7 @@ window.AEMDESIGN.components.twitter = AEMDESIGN.components.twitter || {};
         window.twttr = (function (d,s,id) {
             var t, js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-            js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+            js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
             return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
         }(document, "script", "twitter-wjs"));
 
