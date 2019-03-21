c1822ad8bdc05cc57af8a1675dbcf5f53ca8a2ab
Max Barrass <max.barrass@gmail.com>
2019-03-21 17:38:01 +1100

    add ability to component properties to reevaluate expression fields

---
+++ aemdesign-aem-services/src/main/java/design/aem/components/ComponentField.java
@@ -0,0 +1,108 @@
+package design.aem.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ComponentField {
+
+    private static final long serialVersionUID = -2404935286690975616L;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentField.class);
+
+    private String fieldName;
+    private Object defaultValue;
+    private String dataAttributeName;
+    private String multiValueClass;
+    private String expression;
+    private Object value;
+
+    public ComponentField(String fieldName, Object defaultValue, String dataAttributeName, String mutliValueClass, String expression, Object value) {
+        this.fieldName = fieldName;
+        this.defaultValue = defaultValue;
+        this.dataAttributeName = dataAttributeName;
+        this.multiValueClass = mutliValueClass;
+        this.expression = expression;
+        this.value = value;
+    }
+
+    /**
+     * Create component field using an array.
+     *
+     * @param fieldDefinitionArray Array positions
+     *                             1 fieldName:           required - property name,
+     *                             2 defaultValue:        required - default value,
+     *                             3 dataAttributeName:   optional - name of component attribute to add value into, specifying "" will return values process as per canonical name
+     *                             4 multiValueClass:     optional - canonical name of class for handling multivalues, String or Tag
+     */
+    public ComponentField(Object[] fieldDefinitionArray) {
+        this.fieldName = (String) fieldDefinitionArray[0];
+        this.defaultValue = fieldDefinitionArray[1];
+        if (fieldDefinitionArray.length > 2) {
+            this.dataAttributeName = (String) fieldDefinitionArray[2];
+        }
+        if (fieldDefinitionArray.length > 3) {
+            this.multiValueClass = (String) fieldDefinitionArray[3];
+        }
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public Object getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(Object defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getDataAttributeName() {
+        return dataAttributeName;
+    }
+
+    public void setDataAttributeName(String dataAttributeName) {
+        this.dataAttributeName = dataAttributeName;
+    }
+
+    public String getMultiValueClass() {
+        return multiValueClass;
+    }
+
+    public void setMultiValueClass(String multiValueClass) {
+        this.multiValueClass = multiValueClass;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentField{" +
+                "fieldName='" + fieldName + '\'' +
+                ", defaultValue=" + defaultValue +
+                ", dataAttributeName='" + dataAttributeName + '\'' +
+                ", multiValueClass='" + multiValueClass + '\'' +
+                ", expression='" + expression + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
--- aemdesign-aem-services/src/main/java/design/aem/components/ComponentProperties.java
+++ aemdesign-aem-services/src/main/java/design/aem/components/ComponentProperties.java
@@ -1,90 +1,122 @@
 package design.aem.components;
 
 import com.adobe.granite.ui.components.AttrBuilder;
+import org.apache.commons.jexl3.*;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ComponentProperties extends ValueMapDecorator {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ComponentProperties.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentProperties.class);
 
-  public AttrBuilder attr; //NOSONAR use simpler patter as a util
+    public AttrBuilder attr; //NOSONAR use simpler patter as a util
+    public ArrayList<ComponentField> expressionFields;
 
-  /***
-   * <p>Created empty map.</p>
-   */
-  @SuppressWarnings("unchecked")
-  public ComponentProperties() {
-    super(new HashMap());
-  }
-
-  /***
-   * <p>Creates a new wrapper around a given map.</p>
-   *
-   * @param base wrapped object
-   */
-  public ComponentProperties(Map<String, Object> base) {
-    super(base);
-  }
-
-  public Object get(String name) {
-    return super.get(name);
-  }
-
-  @Override
-  public void putAll(Map<? extends String, ?> map) {
-    if (map != null) {
-      super.putAll(map);
+    /***
+     * <p>Created empty map.</p>
+     */
+    @SuppressWarnings("unchecked")
+    public ComponentProperties() {
+        super(new HashMap());
     }
-  }
 
-  /***
-   * <p>put map into existing map.</p>
-   * @param map new entries
-   * @param update update entries, skip if non blank value exist
-   */
-  public void putAll(Map<? extends String, ?> map, Boolean update) { //NOSONAR some things need to
-    // be complicated
-    if (map != null) {
-      if (update == null || !update) {
-        //on null or not update do putAll
-        super.putAll(map);
-        return;
-      }
-      for (Entry<? extends String, ?> entry : map.entrySet()) {
-        if (super.containsKey(entry.getKey())) {
-          //skip if non blank value exist
-          Object currentValue = super.get(entry.getKey());
-          Object newValue = entry.getValue();
-          if (currentValue != null && newValue != null) {
-            if (newValue.getClass().isArray()) {
-              // if newValue is empty don't do anything
-              if (ArrayUtils.getLength(newValue) == 0) {
-                continue;
-              } else {
-                //test if currentValue is not empty
-                if (currentValue.getClass().isArray() && ArrayUtils.getLength(currentValue) != 0) {
-                  //if its an empty array
-                  LOG.warn("skip: " + entry.getKey() + " current value it not empty array");
-                  continue;
-                }
-              }
-            } else {
-              // if newValue is empty don't do anything
-              if (StringUtils.isEmpty(newValue.toString())) {
-                continue;
-              }
-            }
-          }
+    /***
+     * <p>Creates a new wrapper around a given map.</p>
+     *
+     * @param base wrapped object
+     */
+    public ComponentProperties(Map<String, Object> base) {
+        super(base);
+    }
+
+    public Object get(String name) {
+        return super.get(name);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ?> map) {
+        if (map != null) {
+            super.putAll(map);
         }
-        super.put(entry.getKey(), entry.getValue());
-      }
     }
-  }
+
+    /***
+     * <p>put map into existing map.</p>
+     * @param map new entries
+     * @param update update entries, skip if non blank value exist
+     */
+    public void putAll(Map<? extends String, ?> map, Boolean update) { //NOSONAR some things need to
+        // be complicated
+        if (map != null) {
+            if (update == null || !update) {
+                //on null or not update do putAll
+                super.putAll(map);
+                return;
+            }
+            for (Entry<? extends String, ?> entry : map.entrySet()) {
+                if (super.containsKey(entry.getKey())) {
+                    //skip if non blank value exist
+                    Object currentValue = super.get(entry.getKey());
+                    Object newValue = entry.getValue();
+                    if (currentValue != null && newValue != null) {
+                        if (newValue.getClass().isArray()) {
+                            // if newValue is empty don't do anything
+                            if (ArrayUtils.getLength(newValue) == 0) {
+                                continue;
+                            } else {
+                                //test if currentValue is not empty
+                                if (currentValue.getClass().isArray() && ArrayUtils.getLength(currentValue) != 0) {
+                                    //if its an empty array
+                                    LOGGER.warn("skip: " + entry.getKey() + " current value it not empty array");
+                                    continue;
+                                }
+                            }
+                        } else {
+                            // if newValue is empty don't do anything
+                            if (StringUtils.isEmpty(newValue.toString())) {
+                                continue;
+                            }
+                        }
+                    }
+                }
+                super.put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+
+    public void evaluateExpressionFields() {
+
+        JexlEngine jexl = new JexlBuilder().create();
+        JxltEngine jxlt = jexl.createJxltEngine();
+        JexlContext jc = new MapContext(this);
+//        LOGGER.error("evaluateExpressionFields");
+        if (expressionFields != null) {
+            for (ComponentField field : expressionFields) {
+//                LOGGER.error("evaluateExpressionFields: field={}, prev value={}, name={}, current value={}",field, field.getValue(), field.getFieldName(), this.get(field.getFieldName()));
+                try {
+                    JxltEngine.Expression expr = jxlt.createExpression(field.getDefaultValue().toString());
+                    jc.set("value", this.get(field.getFieldName(),""));
+                    Object expressonResult = expr.evaluate(jc);
+
+                    if (expressonResult != null) {
+//                        LOGGER.error("evaluateExpressionFields: name={}, new value={}", field.getFieldName(), expressonResult);
+                        this.put(field.getFieldName(),expressonResult);
+                        this.attr.set(field.getDataAttributeName(), (String)expressonResult);
+                    }
+                } catch (JexlException jex) {
+                    LOGGER.error("evaluateExpressionFields: field={},JexlException={}", field, jex);
+                } catch (Exception ex) {
+                    LOGGER.error("evaluateExpressionFields: field={},Exception={}", field, ex);
+                }
+            }
+        }
+    }
 }
