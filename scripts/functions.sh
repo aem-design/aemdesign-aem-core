4dd98337f2632dfa42e831a1885153d14230bbe2
Max Barrass <max.barrass@gmail.com>
2019-04-04 01:08:04 +1100

    update script to use PARENT_PROJECT_PATH add functions-common helper update helpers with updates from support repo

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SCRIPTS_PARENT=$(realpath ../scripts)
+CURRENT_PATH="$(pwd)"
+PARENT_PROJECT_PATH="${PARENT_PROJECT_PATH:-$CURRENT_PATH}"
+SCRIPTS_PARENT="${SCRIPTS_PARENT:-$PARENT_PROJECT_PATH}/scripts"
 
+source "$SCRIPTS_PARENT/functions-common.sh"
 source "$SCRIPTS_PARENT/functions-debug.sh"
 source "$SCRIPTS_PARENT/functions-maven.sh"
 source "$SCRIPTS_PARENT/functions-curl.sh"
