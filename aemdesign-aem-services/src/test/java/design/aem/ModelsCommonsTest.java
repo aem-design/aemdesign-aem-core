5542b95e009b5db82d3b5473440007f8a95325c1
Max Barrass <max.barrass@gmail.com>
2019-05-03 21:45:14 +1000

    add common model unit test

---
+++ aemdesign-aem-services/src/test/java/design/aem/ModelsCommonsTest.java
@@ -0,0 +1,29 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2017 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package design.aem;
+
+import com.adobe.cq.wcm.core.components.testing.AbstractModelTest;
+import org.junit.Test;
+
+public class ModelsCommonsTest extends AbstractModelTest {
+
+    @Test
+    public void testDefaultBehaviour() throws Exception {
+        testDefaultBehaviour(new String[] {
+                "design.aem"
+       });
+    }
+}
