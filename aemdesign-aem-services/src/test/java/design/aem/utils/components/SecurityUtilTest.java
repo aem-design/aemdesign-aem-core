39fd68a0a6e8767c7bc3e9ab09a855d6f8e80c0a
Max Barrass <max.barrass@gmail.com>
2019-05-04 01:20:55 +1000

    update security util unit tests

--- aemdesign-aem-services/src/test/java/design/aem/utils/components/SecurityUtilTest.java
+++ aemdesign-aem-services/src/test/java/design/aem/utils/components/SecurityUtilTest.java
@@ -9,13 +9,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SecurityUtilTest {
@@ -42,67 +42,151 @@ public class SecurityUtilTest {
         // Verify the results
         assertNotNull(test);
     }
-//
-//    @Test
-//    public void testIsUserMemberOf() throws Exception {
-//        // Setup
-//        String userPath = "/home/users/a/admin";
-//        final List<String> groups = new ArrayList<String>();
-//        groups.add("administrators");
-//
-//        final List<Group> memberOf = new ArrayList<Group>();
-//
-//
-//        Authorizable adminUserAuth = mock(Authorizable.class);
-//        User adminUser = mock(User.class);
-//        Group administrators = mock(Group.class);
-//
-//        when(adminUser.isAdmin()).thenReturn(true);
-//
-//        memberOf.add(administrators);
-//
-//        when(adminUserAuth.memberOf()).thenReturn(memberOf.iterator());
-//
-//        // Run the test
-//        final boolean result = SecurityUtil.isUserMemberOf(adminUserAuth, groups);
-//
-//        LOGGER.error("result={}", result);
-//
-//        // Verify the results
-//        assertTrue(result);
-//    }
+
+    @Test
+    public void testIsAdminUserMemberOfGroup() throws Exception {
+        // Setup
+        String userPath = "/home/users/a/admin";
+        final List<String> groups = new ArrayList<String>();
+        groups.add("administrators");
+
+        final List<Group> memberOf = new ArrayList<Group>();
 
 
-//
-//    @Test
-//    public void testGetUserEmail() {
-//        // Setup
-//        final UserManager _userManager = null;
-//        final UserPropertiesManager _userPropertiesManager = null;
-//        final String userId = "userId";
-//        final String defaultValue = "defaultValue";
-//        final String expectedResult = "result";
-//
-//        // Run the test
-//        final String result = SecurityUtil.getUserEmail(_userManager, _userPropertiesManager, userId, defaultValue);
-//
-//        // Verify the results
-//        assertEquals(expectedResult, result);
-//    }
-//
-//    @Test
-//    public void testGetUserFullName() {
-//        // Setup
-//        final UserManager _userManager = null;
-//        final UserPropertiesManager _userPropertiesManager = null;
-//        final String userId = "userId";
-//        final String defaultValue = "defaultValue";
-//        final String expectedResult = "result";
-//
-//        // Run the test
-//        final String result = SecurityUtil.getUserFullName(_userManager, _userPropertiesManager, userId, defaultValue);
-//
-//        // Verify the results
-//        assertEquals(expectedResult, result);
-//    }
+        Authorizable adminUserAuth = mock(Authorizable.class, withSettings().extraInterfaces(User.class)); mock(Authorizable.class);
+
+        User adminUser = (User)adminUserAuth;
+
+        when(adminUser.isAdmin()).thenReturn(true);
+
+        Group administrators = mock(Group.class);
+        Principal adminPrinciple = mock(Principal.class);
+
+
+        when(administrators.getPrincipal()).thenReturn(adminPrinciple);
+        when(adminPrinciple.getName()).thenReturn("administrators");
+
+        memberOf.add(administrators);
+
+        when(adminUserAuth.memberOf()).thenReturn(memberOf.iterator());
+
+
+        // Run the test
+        final boolean result = SecurityUtil.isUserMemberOf(adminUserAuth, groups);
+
+        LOGGER.error("result={}", result);
+
+        // Verify the results
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsUserMemberOfGroup() throws Exception {
+        // Setup
+        String userPath = "/home/users/a/admin";
+        final List<String> groups = new ArrayList<String>();
+        groups.add("administrators");
+
+        final List<Group> memberOf = new ArrayList<Group>();
+
+
+        Authorizable adminUserAuth = mock(Authorizable.class, withSettings().extraInterfaces(User.class)); mock(Authorizable.class);
+
+        User adminUser = (User)adminUserAuth;
+
+        when(adminUser.isAdmin()).thenReturn(false);
+
+        Group administrators = mock(Group.class);
+        Group users = mock(Group.class);
+        Principal adminPrinciple = mock(Principal.class);
+        Principal usersPrinciple = mock(Principal.class);
+
+
+        when(administrators.getPrincipal()).thenReturn(adminPrinciple);
+        when(adminPrinciple.getName()).thenReturn("administrators");
+        when(users.getPrincipal()).thenReturn(usersPrinciple);
+        when(usersPrinciple.getName()).thenReturn("users");
+
+        memberOf.add(users);
+        memberOf.add(administrators);
+
+        when(adminUserAuth.memberOf()).thenReturn(memberOf.iterator());
+
+
+        // Run the test
+        final boolean result = SecurityUtil.isUserMemberOf(adminUserAuth, groups);
+
+        LOGGER.error("result={}", result);
+
+        // Verify the results
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsUserWithoutMembershipMemberOfGroup() throws Exception {
+        // Setup
+        String userPath = "/home/users/a/admin";
+        final List<String> groups = new ArrayList<String>();
+        groups.add("administrators");
+
+        final List<Group> memberOf = new ArrayList<Group>();
+
+
+        Authorizable adminUserAuth = mock(Authorizable.class, withSettings().extraInterfaces(User.class)); mock(Authorizable.class);
+
+        User adminUser = (User)adminUserAuth;
+
+        when(adminUser.isAdmin()).thenReturn(false);
+
+        when(adminUserAuth.memberOf()).thenReturn(memberOf.iterator());
+
+
+        // Run the test
+        final boolean result = SecurityUtil.isUserMemberOf(adminUserAuth, groups);
+
+        LOGGER.error("result={}", result);
+
+        // Verify the results
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsUserWithMembershipMemberOfGroup() throws Exception {
+        // Setup
+        String userPath = "/home/users/a/admin";
+        final List<String> groups = new ArrayList<String>();
+        groups.add("administrators");
+
+        final List<Group> memberOf = new ArrayList<Group>();
+
+
+        Authorizable adminUserAuth = mock(Authorizable.class, withSettings().extraInterfaces(User.class)); mock(Authorizable.class);
+
+        User adminUser = (User)adminUserAuth;
+
+        when(adminUser.isAdmin()).thenReturn(false);
+
+        Group users = mock(Group.class);
+        Principal usersPrinciple = mock(Principal.class);
+
+
+        when(users.getPrincipal()).thenReturn(usersPrinciple);
+        when(usersPrinciple.getName()).thenReturn("users");
+
+        memberOf.add(users);
+
+
+        when(adminUserAuth.memberOf()).thenReturn(memberOf.iterator());
+
+
+        // Run the test
+        final boolean result = SecurityUtil.isUserMemberOf(adminUserAuth, groups);
+
+        LOGGER.error("result={}", result);
+
+        // Verify the results
+        assertFalse(result);
+    }
+
+
 }
