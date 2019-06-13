f5ba6e6fd8d2f426e525d226278d02dae3cb6484
wildone <max.barrass@gmail.com>
2019-06-13 17:24:16 +1000

    remove unused code

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/widgets/OnlineMedia.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/widgets/OnlineMedia.java
@@ -28,7 +28,6 @@ public class OnlineMedia extends WCMUsePojo {
 
 
         final String DEFAULT_VARIANT = "default";
-        final String RESOURCE_EXISTS = "resourceExists";
 
         Object[][] componentFields = {
                 {FIELD_VARIANT, DEFAULT_VARIANT},
@@ -45,10 +44,6 @@ public class OnlineMedia extends WCMUsePojo {
                 componentFields,
                 DEFAULT_FIELDS_STYLE,
                 DEFAULT_FIELDS_ACCESSIBILITY);
-
-        String field_variant = componentProperties.get(FIELD_VARIANT,StringUtils.EMPTY);
-        String resourceName = String.format("%s.%s.%s", FIELD_VARIANT, field_variant, EXTENSION_JSP);
-        componentProperties.put(RESOURCE_EXISTS, checkResourceHasChildResource(resourceName, getResource(), getResourceResolver()));
 
     }
 
