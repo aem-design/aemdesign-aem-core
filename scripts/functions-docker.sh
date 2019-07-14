e9674e42c978ac658fba4ac882b597aec559c4f3
wildone <max.barrass@gmail.com>
2019-07-15 00:38:25 +1000

    add helper to output docker container name

--- scripts/functions-docker.sh
+++ scripts/functions-docker.sh
@@ -3,8 +3,12 @@
 DOCKER=$(/usr/bin/which docker)
 AWK=$(/usr/bin/which awk)
 
+function getContainerNameByPort() {
+    local CONTAINER_PORT=${1?Need container port}
+    echo $(docker ps --format='table {{.ID}}\t{{.Names}}\t{{.Ports}}' | grep "$CONTAINER_PORT->" | awk '{print $2}')
+}
 function doRestartContainerUsingPort() {
     local CONTAINER_PORT=${1?Need container port}
-    local CONTAINER_ID=$(docker ps | grep "$CONTAINER_PORT->" | awk '{print $1}')
+    local CONTAINER_ID=$(getContainerNameByPort ${CONTAINER_PORT})
     $DOCKER restart $CONTAINER_ID
 }
