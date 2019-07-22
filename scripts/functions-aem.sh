fad544453877af71b1528dcaf35a1cd319f465a7
wildone <max.barrass@gmail.com>
2019-07-22 15:10:51 +1000

    update core scripts to restart aem after deploy

--- scripts/functions-aem.sh
+++ scripts/functions-aem.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 CURL=$(/usr/bin/which curl)
+CAT=$(/usr/bin/which cat)
 GREP=$(/usr/bin/which grep)
+HEAD=$(/usr/bin/which head)
+TAIL=$(/usr/bin/which tail)
+SED=$(/usr/bin/which sed)
 
 SERVER_ADDRESS="${AEM_SCHEMA}://${AEM_HOST}:${AEM_PORT}"
 LOGIN_DEFAULT="${AEM_USER}:${AEM_PASS}"
@@ -119,5 +123,51 @@ function doCheckPathExist() {
     local PATH=${1?Need path}
 
     doGetCheck "${AEM_USER}:${AEM_PASS}" "${AEM_SCHEMA}://${AEM_HOST}:${AEM_PORT}" "${PATH}"
+
+}
+
+function restart_aem_docker() {
+    if [[ "false" == "$SKIP_RESTART" ]]; then
+        AEM_CONTAINER_NAME=$(getContainerNameByPort "$AEM_PORT")
+        echo "Restarting AEM Docker Container: ${AEM_CONTAINER_NAME}"
+        doRestartContainerUsingPort "$AEM_PORT"
+        #doPostFields "/system/console/vmstat" "-F shutdown_type=Restart"
+    fi
+
+
+
+    set_term_title "Wait for AEM Ready"
+    echo "Waiting for AEM to be ready"
+    doWaitForStartup
+    doWaitForPackageManagerStartup
+    doWaitForBundlesToInstall
+
+    echo "AEM is ready!"
+
+}
+
+
+function delete_current_jar() {
+
+    set_term_title "Get Project Version"
+
+    PROJECT_VERSION=$(getParamOrDefault "" "version")
+    PROJECT_ARTIFACTID=$($CAT "./pom.xml" | $GREP artifactId | $HEAD -n 2 | $TAIL -n 1 | $SED -e 's/.*>\(.*\)<.*/\1/')
+    GIT_VERSION=$(getCurrentProjectVersion)
+
+    echo "Current Project Version: $PROJECT_VERSION"
+    echo "Current Git Version: $GIT_VERSION"
+    echo "Project Artifact Id Version: $PROJECT_ARTIFACTID"
+
+    PROJECT_JAR="/apps/aemdesign/install/${PROJECT_ARTIFACTID}-${GIT_VERSION}.jar"
+    echo "Project Jar: $PROJECT_JAR"
+
+    JAR_EXIST=$(doCheckPathExist "$PROJECT_JAR")
+
+    echo "Check if Jar exists: $JAR_EXIST"
+
+    echo "Delete Current Jar: ${PROJECT_JAR}"
+    doDeletePath "${PROJECT_JAR}"
+    set_term_title "Services Deploy"
 
 }
