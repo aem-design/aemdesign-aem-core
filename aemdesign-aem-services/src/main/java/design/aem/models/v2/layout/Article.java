a22c475db215d8607e736cb2578f00362ae8658d
wildone <max.barrass@gmail.com>
2019-07-29 11:52:02 +1000

    replace javadoc blocks with comment blocks

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/layout/Article.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/layout/Article.java
@@ -19,14 +19,14 @@ public class Article extends ModelProxy {
 
         final String DEFAULT_ARIA_ROLE = "article";
 
-		/**
-		 * Component Fields Helper
-		 *
-		 * Structure:
-		 * 1 required - property name,
-		 * 2 required - default value,
-		 * 3 optional - name of component attribute to add value into
-		 * 4 optional - canonical name of class for handling multivalues, String or Tag
+		/*
+		  Component Fields Helper
+
+		  Structure:
+		  1 required - property name,
+		  2 required - default value,
+		  3 optional - name of component attribute to add value into
+		  4 optional - canonical name of class for handling multivalues, String or Tag
 		 */
         setComponentFields(new Object[][]{
                 {FIELD_ARIA_ROLE, DEFAULT_ARIA_ROLE, DEFAULT_ARIA_ROLE_ATTRIBUTE},
--- aemdesign-aem-services/src/main/java/design/aem/models/v2/layout/Columns.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/layout/Columns.java
@@ -100,14 +100,14 @@ public class Columns extends ModelProxy {
         final String COMPONENT_NAMESPACE_PROPERTIES = ".componentProperties";
         final String COMPONENT_NAMESPACE_CURRENTCOLUMN = ".currentColumn";
 
-		/**
-		 * Component Fields Helper
-		 *
-		 * Structure:
-		 * 1 required - property name,
-		 * 2 required - default value,
-		 * 3 optional - name of component attribute to add value into
-		 * 4 optional - canonical name of class for handling multivalues, String or Tag
+		/*
+		  Component Fields Helper
+
+		  Structure:
+		  1 required - property name,
+		  2 required - default value,
+		  3 optional - name of component attribute to add value into
+		  4 optional - canonical name of class for handling multivalues, String or Tag
 		 */
         setComponentFields(new Object[][]{
                 {FIELD_VARIANT, DEFAULT_VARIANT},
