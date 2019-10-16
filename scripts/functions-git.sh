547a54b2292825d356e1ae42dd801d2520e86666
wildone <max.barrass@gmail.com>
2019-10-16 15:33:46 +1100

    add helper getReleaseNotes

--- scripts/functions-git.sh
+++ scripts/functions-git.sh
@@ -7,3 +7,7 @@ function getCurrentProjectVersion() {
     local GIT_DESCRIBE=($(echo $(git describe) | tr "-" "\n"))
     echo "${GIT_DESCRIBE[0]}.${GIT_DESCRIBE[1]}"
 }
+
+function getReleaseNotes() {
+    git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%h - %s (%an)"
+}
