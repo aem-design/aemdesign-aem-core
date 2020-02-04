fea4506841584acc9a7aed37fbbff3161e96eb5a
wildone <max.barrass@gmail.com>
2020-02-04 21:50:36 +1100

    add missing code for dynamic dialog generator

---
+++ aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/widgets/vue/v2/vue/clientlibs-author/js/field-templates/autocomplete.js
@@ -0,0 +1,63 @@
+window.AEMDESIGN = window.AEMDESIGN || {};
+window.AEMDESIGN.components = AEMDESIGN.components || {};
+window.AEMDESIGN.components.authoring = AEMDESIGN.components.authoring || {};
+window.AEMDESIGN.components.authoring.vue = AEMDESIGN.components.authoring.vue || {};
+window.AEMDESIGN.components.authoring.vue.fields = AEMDESIGN.components.authoring.vue.fields || {};
+
+((ns, undefined) => { // NOSONAR convention for wrapping all modules
+
+  /**
+   * Generates any autocompletion tags needed for the picker component.
+   *
+   * @param {Array|String} values Saved JCR values or a single value
+   * @return {String}
+   */
+  function generateAutocompleteTags(values) {
+    values = values instanceof Array ? values : [values];
+
+    return values.map((value) => (
+      `<coral-tag value="${value}">${value}</coral-tag>`
+    )).join('');
+  }
+
+  ns.autoComplete = (config, fieldPath, labelledBy, savedValue, tagPath) => {
+    const autocompleteElement = document.createElement('div');
+    const needsMultiple       = config.multiple === true;
+    const pickerSource        = encodeURIComponent(`${tagPath}/${config.source}`);
+
+    autocompleteElement.innerHTML = `
+      <foundation-autocomplete
+        class="cq-ui-tagfield"
+        name="${fieldPath}"
+        pickersrc="/mnt/overlay/cq/gui/content/coral/common/form/tagfield/picker.html?root=${pickerSource}&selectionCount=${needsMultiple ? 'multiple' : 'single'}"
+        labelledby="${labelledBy}"
+        valuedisplaymode="block"
+        data-foundation-validation
+        required
+      >
+        <coral-overlay
+          foundation-autocomplete-suggestion
+          class="foundation-picker-buttonlist"
+          data-foundation-picker-buttonlist-src="/mnt/overlay/cq/gui/content/coral/common/form/tagfield/suggestion{.offset,limit}.html?root=${pickerSource}{&amp;query}"
+        ></coral-overlay>
+
+        <coral-taglist foundation-autocomplete-value name="${fieldPath}">
+          ${savedValue && generateAutocompleteTags(savedValue)}
+        </coral-taglist>
+
+        <input class="foundation-field-related" type="hidden" name="${fieldPath}@TypeHint" value="String${needsMultiple ? '[]' : ''}">
+        <input class="foundation-field-related" type="hidden" name="${fieldPath}s@Delete">
+
+        <ui:includeClientLib categories="cq.ui.coral.common.tagfield" />
+      </foundation-autocomplete>
+    `;
+
+    const autocomplete = autocompleteElement.children[0];
+
+    autocomplete.invalid  = !savedValue || false;
+    autocomplete.multiple = needsMultiple;
+
+    return autocomplete;
+  };
+
+})(AEMDESIGN.components.authoring.vue.fields);
---
+++ aemdesign-aem-common/src/main/content/jcr_root/apps/aemdesign/components/widgets/vue/v2/vue/clientlibs-author/js/field-templates/path-browser.js
@@ -0,0 +1,42 @@
+window.AEMDESIGN = window.AEMDESIGN || {};
+window.AEMDESIGN.components = AEMDESIGN.components || {};
+window.AEMDESIGN.components.authoring = AEMDESIGN.components.authoring || {};
+window.AEMDESIGN.components.authoring.vue = AEMDESIGN.components.authoring.vue || {};
+window.AEMDESIGN.components.authoring.vue.fields = AEMDESIGN.components.authoring.vue.fields || {};
+
+((ns, undefined) => { // NOSONAR convention for wrapping all modules
+
+  ns.pathBrowser = (config, fieldPath, labelledBy, savedValue) => {
+    const pathbrowserElement = document.createElement('div');
+
+    pathbrowserElement.innerHTML = `
+      <span class="coral-PathBrowser"
+        data-init="pathbrowser"
+        data-root-path="/content"
+        data-option-loader="granite.ui.pathBrowser.pages.hierarchyNotFile"
+        data-picker-src="/libs/wcm/core/content/common/pathbrowser/column.html/content?predicate=hierarchyNotFile"
+        data-picker-title="Select Path"
+        data-crumb-root="content"
+        data-picker-multiselect="false"
+        data-root-path-valid-selection="true">
+        <span class="coral-InputGroup coral-InputGroup--block">
+          <input class="coral-InputGroup-input coral-Textfield js-coral-pathbrowser-input"
+            type="text"
+            name="${fieldPath}"
+            autocomplete="off"
+            aria-labelledby="${labelledBy}"
+            value="${savedValue !== undefined ? savedValue : ''}"
+            data-validation>
+          <span class="coral-InputGroup-button">
+            <button class="coral-Button coral-Button--square js-coral-pathbrowser-button" type="button" title="Browse">
+              <i class="coral-Icon coral-Icon--sizeS coral-Icon--folderSearch"></i>
+            </button>
+          </span>
+        </span>
+      </span>
+    `;
+
+    return pathbrowserElement.children[0];
+  };
+
+})(AEMDESIGN.components.authoring.vue.fields);
