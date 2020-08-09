5e22e12851f7a3676d8f1a0a0999a257f89f9b2f
Chris Shaw <me@cshaw.xyz>
2020-08-10 00:43:30 +1000

    Upgrade preperation work to PageList

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/PageList.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/PageList.java
@@ -1,23 +1,25 @@
 package design.aem.models.v2.lists;
 
-import com.day.cq.i18n.I18n;
-
-import static design.aem.utils.components.I18nUtil.*;
-
 public class PageList extends List {
+    static {
+        DETAILS_COMPONENT_LOOKUP_NAMES = new String[]{
+            "page-details",
+            "generic-details",
+        };
+    }
+
+
     private final String DEFAULT_I18N_CATEGORY = "pagelist";
 
-    @Override
-    @SuppressWarnings("Duplicates")
-    public void ready() {
-        I18n i18n = new I18n(getRequest());
-
-        detailsNameSuffix = new String[]{"page-details", "generic-details"};
-
-        loadConfig();
-
-        //override properties
-        getComponentProperties().put(LISTITEM_LINK_TEXT, getDefaultLabelIfEmpty("", DEFAULT_I18N_CATEGORY, DEFAULT_I18N_LIST_ITEM_LINK_TEXT, DEFAULT_I18N_CATEGORY, i18n));
-        getComponentProperties().put(LISTITEM_LINK_TITLE, getDefaultLabelIfEmpty("", DEFAULT_I18N_CATEGORY, DEFAULT_I18N_LIST_ITEM_LINK_TITLE, DEFAULT_I18N_CATEGORY, i18n));
-    }
+//    @Override
+//    @SuppressWarnings("Duplicates")
+//    public void ready() {
+//        I18n i18n = new I18n(getRequest());
+//
+////        loadConfig();
+//
+//        //override properties
+//        getComponentProperties().put(LISTITEM_LINK_TEXT, getDefaultLabelIfEmpty("", DEFAULT_I18N_CATEGORY, DEFAULT_I18N_LIST_ITEM_LINK_TEXT, DEFAULT_I18N_CATEGORY, i18n));
+//        getComponentProperties().put(LISTITEM_LINK_TITLE, getDefaultLabelIfEmpty("", DEFAULT_I18N_CATEGORY, DEFAULT_I18N_LIST_ITEM_LINK_TITLE, DEFAULT_I18N_CATEGORY, i18n));
+//    }
 }
