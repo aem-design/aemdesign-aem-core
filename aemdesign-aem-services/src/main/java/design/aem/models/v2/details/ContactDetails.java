2ff8dd918806d882a4d06b7de92e388eda06ec6f
wildone <max.barrass@gmail.com>
2021-02-06 07:10:47 +1100

    remove common variables ready: move attributes processing into processComponentFields

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/details/ContactDetails.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/details/ContactDetails.java
@@ -1,26 +1,23 @@
 package design.aem.models.v2.details;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-import static design.aem.utils.components.ComponentsUtil.DETAILS_DESCRIPTION;
-import static design.aem.utils.components.ComponentsUtil.compileComponentMessage;
+import static design.aem.utils.components.ComponentsUtil.*;
+import static design.aem.utils.components.ConstantsUtil.FIELD_PAGE_TITLE;
+import static design.aem.utils.components.ConstantsUtil.FIELD_PAGE_TITLE_NAV;
 import static design.aem.utils.components.TagUtil.getTagValueAsAdmin;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class ContactDetails extends GenericDetails {
     protected static final Logger LOGGER = LoggerFactory.getLogger(ContactDetails.class);
 
     protected static final String COMPONENT_DETAILS_NAME = "contact-details";
 
-    protected static final String FIELD_FORMAT_TITLE = "titleFormat";
-    protected static final String FIELD_FORMAT_DESCRIPTION = "descriptionFormat";
-    protected static final String FIELD_FORMATTED_TITLE = "titleFormatted";
-    protected static final String FIELD_FORMATTED_TITLE_TEXT = "titleFormattedText";
-    protected static final String FIELD_FORMATTED_DESCRIPTION = "descriptionFormatted";
     protected static final String FIELD_HONORIFIC_PREFIX = "honorificPrefix";
 
     protected static final String DEFAULT_FORMAT_TITLE = "${honorificPrefix} ${givenName} ${familyName}";
@@ -30,19 +27,6 @@ public class ContactDetails extends GenericDetails {
     protected void ready() {
         super.ready();
 
-        // Grab value for prefix
-        componentProperties.put(FIELD_HONORIFIC_PREFIX, getTagValueAsAdmin(
-            componentProperties.get(FIELD_HONORIFIC_PREFIX, StringUtils.EMPTY),
-            getSlingScriptHelper()));
-
-        componentProperties.put(DETAILS_DESCRIPTION,
-            componentProperties.get(FIELD_FORMATTED_DESCRIPTION, StringUtils.EMPTY));
-
-        // Set something if title formatted is empty
-        if (isEmpty(componentProperties.get(FIELD_FORMATTED_TITLE, StringUtils.EMPTY))) {
-            componentProperties.put(FIELD_FORMATTED_TITLE, componentProperties.get(FIELD_TITLE, StringUtils.EMPTY));
-            componentProperties.put(FIELD_FORMATTED_TITLE_TEXT, componentProperties.get(FIELD_TITLE, StringUtils.EMPTY));
-        }
     }
 
     @Override
@@ -71,12 +55,39 @@ public class ContactDetails extends GenericDetails {
         Map<String, Object> newFields = super.processComponentFields();
 
         try {
+
+            // Grab value for prefix
+            newFields.put(FIELD_HONORIFIC_PREFIX, getTagValueAsAdmin(
+                componentProperties.get(FIELD_HONORIFIC_PREFIX, StringUtils.EMPTY),
+                getSlingScriptHelper()));
+
+            String formattedTitle = compileComponentMessage(FIELD_FORMAT_TITLE, DEFAULT_FORMAT_TITLE, componentProperties, slingScriptHelper);
+            Document fragment = Jsoup.parse(formattedTitle);
+            String formattedTitleText = fragment.text();
+
+            newFields.put(FIELD_FORMATTED_TITLE,
+                formattedTitle.trim()
+            );
+            newFields.put(FIELD_FORMATTED_TITLE_TEXT,
+                formattedTitleText.trim()
+            );
+
+            newFields.put(FIELD_TITLE, formattedTitleText.trim());
+            newFields.put(FIELD_PAGE_TITLE, formattedTitleText.trim());
+            newFields.put(FIELD_PAGE_TITLE_NAV, formattedTitleText.trim());
+            newFields.put(DETAILS_LINK_TEXT, formattedTitleText.trim());
+            newFields.put(DETAILS_BADGE_TITLE, formattedTitleText.trim());
+            newFields.put(DETAILS_LINK_TITLE, formattedTitleText.trim());
+            newFields.put(DETAILS_ANALYTICS_LABEL, formattedTitleText.trim());
+            newFields.put(DETAILS_BADGE_ANALYTICS_LABEL, formattedTitleText.trim());
+
             newFields.put(FIELD_FORMATTED_DESCRIPTION, compileComponentMessage(
                 FIELD_FORMAT_DESCRIPTION,
                 DEFAULT_FORMAT_DESCRIPTION,
                 componentProperties,
                 slingScriptHelper
             ).trim());
+
         } catch (Exception ex) {
             LOGGER.error("Could not process component fields in {}", COMPONENT_DETAILS_NAME);
         }
