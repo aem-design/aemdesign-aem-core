bf525e8c08a334bef550b9fbfae8696eeb61cff8
wildone <max.barrass@gmail.com>
2021-02-11 02:35:12 +1100

    add schema fields

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/details/PageDetails.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/details/PageDetails.java
@@ -3,6 +3,9 @@ package design.aem.models.v2.details;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static design.aem.utils.components.ComponentsUtil.DETAILS_DATA_SCHEMA_ITEMSCOPE;
+import static design.aem.utils.components.ComponentsUtil.DETAILS_DATA_SCHEMA_ITEMTYPE;
+
 public class PageDetails extends GenericDetails {
     protected static final Logger LOGGER = LoggerFactory.getLogger(PageDetails.class);
 
@@ -12,4 +15,15 @@ public class PageDetails extends GenericDetails {
     protected String getComponentCategory() {
         return "page-detail";
     }
+
+    @Override
+    protected void setFields() {
+        super.setFields();
+
+        setComponentFields(new Object[][]{
+            {DETAILS_DATA_SCHEMA_ITEMSCOPE, "true", DETAILS_DATA_SCHEMA_ITEMSCOPE},
+            {DETAILS_DATA_SCHEMA_ITEMTYPE, "http://schema.org/WebPage"},
+        });
+    }
+
 }
