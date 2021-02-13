02f25130f0c339829d945eb0b297e506ab32fc95
wildone <max.barrass@gmail.com>
2021-02-13 23:01:56 +1100

    update default badge to action

--- aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/ListNav.java
+++ aemdesign-aem-services/src/main/java/design/aem/models/v2/lists/ListNav.java
@@ -57,6 +57,7 @@ public class ListNav extends BaseComponent {
     protected static final boolean LIST_LOOP_DEFAULT = false;
 
     protected static final String DETAILS_BADGE = "detailsBadge";
+    protected static final String DETAILS_BADGE_DEFAULT = "action";
 
     protected String PARENT_PATH_DEFAULT = "/content";
 
@@ -85,7 +86,7 @@ public class ListNav extends BaseComponent {
         sortOrder = SortOrder.fromString(componentProperties.get(PN_SORT_ORDER, SortOrder.ASC.getValue()));
         limit = componentProperties.get(LIMIT_PROPERTY_NAME, LIMIT_DEFAULT);
         listLoop = componentProperties.get(LIST_LOOP, LIST_LOOP_DEFAULT);
-        detailsBadge = componentProperties.get(DETAILS_BADGE, DEFAULT_BADGE);
+        detailsBadge = componentProperties.get(DETAILS_BADGE, DETAILS_BADGE_DEFAULT);
 
         String filterPagePage = componentProperties.get("filterPage", StringUtils.EMPTY);
 
@@ -110,7 +111,7 @@ public class ListNav extends BaseComponent {
             {FIELD_IMAGE_OPTION, FIELD_IMAGE_OPTION_DEFAULT},
             {LIST_LOOP, LIST_LOOP_DEFAULT},
             {"filterPage", StringUtils.EMPTY},
-            {DETAILS_BADGE, DEFAULT_BADGE, "data-badge"},
+            {DETAILS_BADGE, DETAILS_BADGE_DEFAULT, "data-badge"},
         });
     }
 
